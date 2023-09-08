c409e6b33feb76d60b11c313c99e3164baef15cf
tenma <tenma@osxt.com>
2023-09-08 19:06:56 +0200

    test fix images

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,20 +2,20 @@ import "./SideNav.css";
 
 /** Side nav of the website*/
 export const Sidenav = () => {
-    const navItems = ["Bike", "Yoga", "Swim", "Dumbell"];
-    const iconSrc = (icon) => `/SideNav/${icon}.png`;
-    return (
-        <div className="side-nav">
-            <nav>
-                <ul>
-                    {navItems.map((item, key) => (
-                        <li key={key}>
-                            <img src={iconSrc(item)} alt={item} />
-                        </li>
-                    ))}
-                </ul>
-                <p>Copyright, Sportsee 2022</p>
-            </nav>
-        </div>
-    );
+  const navItems = ["Bike", "Yoga", "Swim", "Dumbell"];
+  const iconSrc = (icon) => `/public/SideNav/${icon}.png`;
+  return (
+    <div className="side-nav">
+      <nav>
+        <ul>
+          {navItems.map((item, key) => (
+            <li key={key}>
+              <img src={iconSrc(item)} alt={item} />
+            </li>
+          ))}
+        </ul>
+        <p>Copyright, Sportsee 2022</p>
+      </nav>
+    </div>
+  );
 };
--- src/containers/Profil/UserData/UserData.jsx
+++ src/containers/Profil/UserData/UserData.jsx
@@ -5,61 +5,61 @@ import PropTypes from "prop-types";
  * @param {object} keyData - user key infos
  */
 export const UserData = ({ keyData }) => {
-    /** Get the icon image url
-     *  @param {string} icon - name of the icon fot find it in assets
-     */
-    const iconLink = (icon) => `/KeyData/${icon}.png`;
+  /** Get the icon image url
+   *  @param {string} icon - name of the icon fot find it in assets
+   */
+  const iconLink = (icon) => `/public/KeyData/${icon}.png`;
 
-    const items = [
-        {
-            param: "calories",
-            label: "Calories",
-            icon: iconLink("Calories"),
-            value: keyData?.calorieCount,
-            unity: "kCal",
-        },
-        {
-            param: "protein",
-            label: "Protéines",
-            icon: iconLink("Protein"),
-            value: keyData?.proteinCount,
-            unity: "g",
-        },
-        {
-            param: "glucid",
-            label: "Glucide",
-            icon: iconLink("Glucid"),
-            value: keyData?.carbohydrateCount,
-            unity: "g",
-        },
-        {
-            param: "lipid",
-            label: "Lipides",
-            icon: iconLink("Lipid"),
-            value: keyData?.lipidCount,
-            unity: "g",
-        },
-    ];
-    return (
-        <div className="user-data">
-            {items.map((item) => (
-                <div key={item.param} className="user-data__card">
-                    <div className={`user-data__card__icon ${item.param}`}>
-                        <img src={item.icon} alt={item.label} />
-                    </div>
-                    <div>
-                        <p className="user-data__card__value">
-                            {item.value}
-                            {item.unity}
-                        </p>
-                        <p className="user-data__card__label">{item.label}</p>
-                    </div>
-                </div>
-            ))}
+  const items = [
+    {
+      param: "calories",
+      label: "Calories",
+      icon: iconLink("Calories"),
+      value: keyData?.calorieCount,
+      unity: "kCal",
+    },
+    {
+      param: "protein",
+      label: "Protéines",
+      icon: iconLink("Protein"),
+      value: keyData?.proteinCount,
+      unity: "g",
+    },
+    {
+      param: "glucid",
+      label: "Glucide",
+      icon: iconLink("Glucid"),
+      value: keyData?.carbohydrateCount,
+      unity: "g",
+    },
+    {
+      param: "lipid",
+      label: "Lipides",
+      icon: iconLink("Lipid"),
+      value: keyData?.lipidCount,
+      unity: "g",
+    },
+  ];
+  return (
+    <div className="user-data">
+      {items.map((item) => (
+        <div key={item.param} className="user-data__card">
+          <div className={`user-data__card__icon ${item.param}`}>
+            <img src={item.icon} alt={item.label} />
+          </div>
+          <div>
+            <p className="user-data__card__value">
+              {item.value}
+              {item.unity}
+            </p>
+            <p className="user-data__card__label">{item.label}</p>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 UserData.propTypes = {
-    keyData: PropTypes.object,
+  keyData: PropTypes.object,
 };
